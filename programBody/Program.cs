43e9ea3e448d390f83fb3c48311a5092379ed7a3
Anatoly Ometov <Fun_spawn@mail.ru>
2024-05-11 14:41:13 +0400

    deleted part of irrelevant code

--- programBody/Program.cs
+++ programBody/Program.cs
@@ -25,23 +25,12 @@ void Main()
 string[] homework = GenerateArray(5);
    PrintArray(homework);
    string[] newHomework = homework.Where(h => h.Length <= 3).ToArray();
-   int count = 0;
    foreach (string work in newHomework)
       Console.WriteLine("Элементы, удовлетворяющие критерию: " + work);
-      count++;
- string [] GenerateNewArray(int count)
-            {
-                string[] newTempArray = new string[count];
-                for (int j = 0; j < count; j++)
-                {
-                    newTempArray = newHomework;
-                    
-                }
-               return newTempArray;
                
 
-            }
-PrintArray(GenerateNewArray(count));
+            
+PrintArray(newHomework);
    }
 
 Main();
