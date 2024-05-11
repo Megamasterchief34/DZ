76078c2877b8dc36e9b76d3a59f88cabe3bec0a5
Anatoly Ometov <Fun_spawn@mail.ru>
2024-05-11 11:56:51 +0400

    added part of main body program

--- programBody/Program.cs
+++ programBody/Program.cs
@@ -22,7 +22,13 @@ void Main()
     }
 
 }
-
+string[] homework = GenerateArray(5);
+   PrintArray(homework);
+   string[] newHomework = homework.Where(h => h.Length <= 3).ToArray();
+   int count = 0;
+   foreach (string work in newHomework)
+      Console.WriteLine("Элементы, удовлетворяющие критерию: " + work);
+      count++;
  string [] GenerateNewArray(int count)
             {
                 string[] newTempArray = new string[count];
